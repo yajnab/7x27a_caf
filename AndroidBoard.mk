e11f8670faba6b93a12d4952c99996adc84053b6
Neti Ravi Kumar <ravineti@codeaurora.org>
2012-06-04 10:29:46 +0530

    Use target specific properties for rendering properties
    Use different properties for 7x27A, 7x25A, and 8625, to enable/disable
    tiled rendering, and dirty region rendering.
    
    Currently hwui.render_dirty_regions, and debug.enabletr are TRUE for
    8x25, for 7x27A/7x25A these properties will be updated, based on
    performance dashboard results.
    
    (cherry picked from commit 8f41869a1aabf284e4bbbc9dfbe4969480ad2120)
    
    Conflicts:
    
    	AndroidBoard.mk
    	system.prop
    
    Change-Id: I4738defe0d42fcfc488ce85898afdcee65409f62

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -100,6 +100,13 @@ include $(BUILD_PREBUILT)
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rendering.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
