918c3d973434e642c45a97db1eca042d8f05beaa
Atul Raut <araut@codeaurora.org>
2012-08-27 11:27:23 +0530

    msm7627a : Enable Camera, Bluetooth & GPS module
    Change-Id: I4084edc0e0ef4ec4ae259306c5f58da257e5c4d3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,13 +22,13 @@ ifeq ($(QC_PROP),true)
     MM_AUDIO_VOEM_DISABLED := false
     MM_AUDIO_MVS_DISABLED :=true
     ifneq ($(BUILD_TINY_ANDROID), true)
-   # BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50001
-   # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-    #BOARD_HAVE_BLUETOOTH := false
+    BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50001
+    BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+    BOARD_HAVE_BLUETOOTH := true
     BOARD_HAVE_QCOM_FM := true
     PROTEUS_DEVICE_API := true
     #HACK use Camera Stub
-    #BOARD_CAMERA_LIBRARIES := libcamera
+    BOARD_CAMERA_LIBRARIES := libcamera
     # Define respective statement to true to enable the ATH or WCN WLAN.
     BOARD_HAS_QCOM_WLAN := true
     BOARD_HAS_ATH_WLAN := false
